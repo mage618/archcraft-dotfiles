0ecb78a96f407666ede9aa2cd4486e4dd04f0821
hyy <yh8953@utexas.edu>
2023-03-13 13:48:28 -0500

    base dots

---
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -0,0 +1,7 @@
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    transparent_background = true,
+})
+
+vim.o.background = "dark"
+vim.cmd.colorscheme('catppuccin')
